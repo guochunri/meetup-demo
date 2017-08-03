2cef209ea5f9b96aa5a0162cc182891bc8cb75a3
郭春日 <uochunri@gmail.com>
2017-08-03 10:38:57 +0800

    Implement AuthController

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  namespace :api, :defaults => { :format => :json } do
+    namespace :v1 do
+      post "/signup" => "auth#signup"
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
+    end
+  end
+
 end
